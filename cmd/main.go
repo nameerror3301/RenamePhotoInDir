2453e7bbafe6f20022be513afa81b1286520985d
root <nameerrror3301@gmail.com>
2022-10-10 14:34:44 +0200

    Covered everything with tests

--- cmd/main.go
+++ cmd/main.go
@@ -13,23 +13,21 @@ import (
 )
 
 func main() {
-	path := "your path to files photo"
-	if status := RenameRandom(path); !status {
-		log.Fatal("Err random rename file")
+	path := "/home/onebyteforlife/go/src/RenamePhotoInDir/Wallpapers"
+	if err := RenameRandom(path); err != nil {
+		log.Fatalf("%s", err)
 	}
 
 	if err := RenameFileInDir(path); err != nil {
-		log.Fatal(err)
+		log.Fatalf("%s", err)
 	}
+
 }
 
 // A final function that combines all functions and checks for a directory
 func RenameFileInDir(path string) error {
-	if _, err := os.Stat(path); err != nil {
-		return fmt.Errorf("The directory does not exist - %s", err)
-	}
 	if err := ReadFileDir(path); err != nil {
-		return err
+		return fmt.Errorf("err rename file in dir - %s", err)
 	}
 	return nil
 }
@@ -37,7 +35,10 @@ func RenameFileInDir(path string) error {
 // Recursive search and renaming files to normal names
 func ReadFileDir(path string) error {
 	var idx int
-	files, _ := os.ReadDir(path)
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf("err read dir - %s", err)
+	}
 	for _, file := range files {
 		if file.IsDir() {
 			ReadFileDir(filepath.Join(path, file.Name()))
@@ -45,7 +46,9 @@ func ReadFileDir(path string) error {
 			if isImage(file.Name()) {
 				idx++
 				shared := filepath.Ext(filepath.Join(path, file.Name()))
-				os.Rename(filepath.Join(path, file.Name()), filepath.Join(path, fmt.Sprintf("Images-%s%s", strconv.Itoa(idx), shared)))
+				if err := os.Rename(filepath.Join(path, file.Name()), filepath.Join(path, fmt.Sprintf("Images-%s%s", strconv.Itoa(idx), shared))); err != nil {
+					return fmt.Errorf("err rename file - %s", err)
+				}
 			}
 		}
 	}
@@ -53,7 +56,10 @@ func ReadFileDir(path string) error {
 }
 
 // Initial renaming of files to random names
-func RenameRandom(path string) bool {
+func RenameRandom(path string) error {
+	if _, err := os.ReadDir(path); err != nil {
+		return err
+	}
 	var pathToFile string
 	filepath.Walk(path, func(wPath string, info fs.FileInfo, err error) error {
 		if info.IsDir() {
@@ -61,14 +67,13 @@ func RenameRandom(path string) bool {
 		}
 		if isImage(wPath) {
 			shared := filepath.Ext(wPath)
-			err := os.Rename(wPath, filepath.Join(pathToFile, fmt.Sprintf("%s%s", GenRandomString(), shared)))
-			if err != nil {
-				fmt.Println(err)
+			if err := os.Rename(wPath, filepath.Join(pathToFile, fmt.Sprintf("%s%s", GenRandomString(), shared))); err != nil {
+				return err
 			}
 		}
 		return nil
 	})
-	return true
+	return nil
 }
 
 // Generating a random string for file names
---
+++ cmd/main_test.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"testing"
+)
+
+// Test func isImage()
+func TestIsImage(t *testing.T) {
+	t.Run("check-IsImage-valid", func(t *testing.T) {
+		var i int = 0
+		var shareds = []string{"one.jpg", "two.jpeg", "three.png"}
+		for _, val := range shareds {
+			status := isImage(val)
+			if status {
+				i++
+			}
+		}
+		if i != 3 {
+			t.Error("isImege() - Did not pass the test with the correct data")
+		}
+	})
+
+	t.Run("check-IsImage-invalid", func(t *testing.T) {
+		var i int = 0
+		var shareds = []string{"one.jpag", "two.j1peg", "three.pn6g"}
+		for _, val := range shareds {
+			status := isImage(val)
+			if status {
+				i++
+			}
+		}
+		if i != 0 {
+			t.Error("isImege() - Didn't pass the test with the wrong data ")
+		}
+	})
+}
+
+func TestReadFileDir(t *testing.T) {
+	t.Run("check-ReadFileDir-valid", func(t *testing.T) {
+		path := "/home/onebyteforlife/go/src/RenamePhotoInDir/Wallpapers"
+		if err := ReadFileDir(path); err != nil {
+			t.Error("RenameFileDir() - Did not pass the test with the correct data")
+		}
+	})
+
+	t.Run("check-ReadFileDir-invalid", func(t *testing.T) {
+		path := ""
+		if err := ReadFileDir(path); err == nil {
+			t.Error("RenameFileDir() - Didn't pass the test with the wrong data")
+		}
+	})
+}
+
+func TestRenameRandom(t *testing.T) {
+	t.Run("check-RenameRandom-valid", func(t *testing.T) {
+		path := "/home/onebyteforlife/go/src/RenamePhotoInDir/Wallpapers"
+		if err := RenameRandom(path); err != nil {
+			t.Error("RenameRandom() - Did not pass the test with the correct data")
+		}
+	})
+
+	t.Run("check-RenameRandom-invalid", func(t *testing.T) {
+		path := ""
+		if err := RenameRandom(path); err == nil {
+			t.Error("RenameRandom() - Didn't pass the test with the wrong data")
+		}
+	})
+}
+
+func TestRenameFileInDir(t *testing.T) {
+	t.Run("check-RenameRandom-valid", func(t *testing.T) {
+		path := "/home/onebyteforlife/go/src/RenamePhotoInDir/Wallpapers"
+		if err := RenameFileInDir(path); err != nil {
+			t.Error("RenameRandom() - Did not pass the test with the correct data")
+		}
+	})
+
+	t.Run("check-RenameRandom-invalid", func(t *testing.T) {
+		path := ""
+		if err := RenameFileInDir(path); err == nil {
+			t.Error("RenameRandom() - Didn't pass the test with the wrong data")
+		}
+	})
+}
